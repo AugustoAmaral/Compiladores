69f43a28c9230da1f6e1a3733b70860bf13aa832
Augusto Amaral Pereira <guto_wow@hotmail.com>
2018-09-17 13:30:01 -0300

    Primeiro
    Programa que yuri me mandou pelo whats

---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define tam_palavras_reservadas 12
+const char* const palavras_reservadas[] = { "+","-","*","/","<-","loop","end","goto","read","write","begin","int" }; //13 palavras reservadas
+
+
+void identificarPalavrasReservadas(char* linha) {
+	printf("\n");
+	char* palavra = strtok(linha, "\t; ()\n");
+	while (palavra != NULL) {
+		for (int i = 0; i<tam_palavras_reservadas; i++) //VERIFICA SE A PALAVRA ATUAL � RESERVADA
+			if (strcmp(palavra, palavras_reservadas[i]) == 0)
+				printf("palavra reservada encontrada: %s \n ", palavra);
+
+		palavra = strtok(NULL, "\t; ()\n");
+	}
+
+}
+
+void pegarLinhas(char* nome_do_arquivo) {
+	char linha[200];
+	FILE *arq;
+	arq = fopen(nome_do_arquivo, "r");
+	if (arq == NULL)
+		printf("Erro, nao foi possivel abrir o arquivo.\n");
+	else
+		while ((fgets(linha, sizeof(linha), arq)) != NULL)//VAI GUARDAR AS LINHAS DO CODIGO EM UM VETOR DE LINHAS
+			identificarPalavrasReservadas(linha);
+	fclose(arq);
+}
+
+int main() {
+	//char arq_nome[50];
+	//printf("digite o nome do arquivo: ");
+	//scanf("%s",arq_nome);
+	char arq_nome[] = "teste.txt";
+	pegarLinhas(arq_nome);
+	return 0;
+}
