248a99ebafa42cca86ea16a8eef4a8da73954194
Augusto Amaral Pereira <guto_wow@hotmail.com>
2018-09-21 14:33:01 -0300

    Revert "Correção de bugs"
    This reverts commit 81c7c1d1db8e236dac5f53d0165ec1ef32af203b.

--- funcarquivos.cpp
+++ funcarquivos.cpp
@@ -2,32 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include "structure.cpp"
-#define tam_palavras_reservadas 12
-const char* const palavras_reservadas[] = { "+","-","*","/","<-","loop","end","goto","read","write","begin","int" }; //13 palavras reservadas
+#define tam_palavras_reservadas 16
+const char* const palavras_reservadas[] = {"program","integer","real","begin","end","goto","loop","read","write","var","<-",";","+","0","1",":"}; //13 palavras reservadas
 
 
-void identificarPalavrasReservadas(char* linha) {
-	printf("\n");
-	char* palavra = strtok(linha, "\t; ()\n");
-	while (palavra != NULL) {
+void identificarPalavrasReservadas(Palavras* linha) {
+	Palavras* p = linha;
+	while (p != NULL) {
 		for (int i = 0; i<tam_palavras_reservadas; i++) //VERIFICA SE A PALAVRA ATUAL ï¿½ RESERVADA
-			if (strcmp(palavra, palavras_reservadas[i]) == 0)
-				printf("palavra reservada encontrada: %s \n", palavra);
-				
-		palavra = strtok(NULL, "\t; ()\n");
+			if (strcmp(p->info, palavras_reservadas[i]) == 0)
+				printf("palavra reservada encontrada: %s \n", p->info);
+		//printf("Palavra: %s ",p->info); DEBUG ONLY
+		p = p->prox;
 	}
 
+}
+
+Palavras* pegarPalavras(char* linha){
+	
+	Palavras* p = (Palavras*) malloc (sizeof(Palavras));
+	p->ant=p->prox=NULL; //inicia estrutura de palavras
+	p->info=NULL;
+	p->id=1;
+	Palavras* palavra_retornada = p;
+	int contador=1;
+	
+	char* palavra = strtok(linha, "\t; ()\n");
+	while (palavra != NULL) {
+		//define a palavra e define a coluna
+		p->info = (char*) malloc (sizeof(palavra));
+		strcpy(p->info,palavra);
+		//printf("%s ",p->info); //DEBUG ONLY
+		p->id = contador;
+		//aloca uma proxima palavra e avança a estrutura
+		p->prox = (Palavras*) malloc (sizeof(Palavras));
+		Palavras* temp = p;
+		p=p->prox;
+		p->ant=temp;
+		p->prox=NULL;
+		//passa para a proxima palavra
+		palavra = strtok(NULL, "\t; ()");
+	}
+	
+	return palavra_retornada;
 }
 
 Linhas* pegarLinhas(char* nome_do_arquivo) {
 	char linha[200];
 
 	//INICIANDO A ESTRUTURA DE LINHAS
-	Linhas* l = (Linhas*) malloc (sizeof(Linhas));
-	l->ant=l->prox= NULL;
-	l->info=NULL;
-	l->id=1;
-	Linhas* linha_retornada = l;
+	Linhas* l = (Linhas*) malloc (sizeof(Linhas)); // Cria uma estrutura de linhas genéricas
+	l->ant=l->prox= NULL; //inicia a estrutura
+	l->info=NULL; // mesma coisa
+	l->id=1; // msm coisa
+	Linhas* linha_retornada = l; // é o que retorna obviamente
 	int contador=1;
 	
 	FILE *arq;
@@ -37,10 +65,10 @@ Linhas* pegarLinhas(char* nome_do_arquivo) {
 	else
 		while ((fgets(linha, sizeof(linha), arq)) != NULL){//VAI GUARDAR AS LINHAS DO CODIGO EM UM VETOR DE LINHAS
 			
-			l->info = (char*) malloc (sizeof(linha)); // ALOCA ESPAÃ‡O PARA A INFO DA LINHA
-			strcpy(l->info,linha); //&l->info = linha; //ARMAZENA A LINHA
+			l->info = (Palavras*) malloc (sizeof(Palavras)); // ALOCA ESPAÃ‡O PARA A INFO DA LINHA
+			l->info = pegarPalavras(strtok(linha,"\n"));
+			//l->info = strtok(linha,"\n"); //&l->info = linha; //ARMAZENA A LINHA
 			l->id = contador; // DEFINE A LINHA DO PROGRAMA
-
 						
 			l->prox = (Linhas*) malloc (sizeof(Linhas)); // CRIA UMA NOVA ESTRUTURA PARA UMA NOVA LINHA
 			Linhas* temp = l; // SALVA O ENDEREÃ‡O DA LINHA ATUAL
@@ -49,7 +77,7 @@ Linhas* pegarLinhas(char* nome_do_arquivo) {
 			l->prox = NULL; // PROXIMO VAI APONTAR PRA NULL
 			contador++; // CONTINUA CONTADOR
 			//identificarPalavrasReservadas(linha);
-			printf("\n");
+			//printf("\n");
 		}
 	fclose(arq);
 	
--- structure.cpp
+++ structure.cpp
@@ -9,10 +9,17 @@ FUNCOES
 END*/
 #include <stdio.h>
 
+typedef struct palavras{
+	struct palavras* ant;
+	int id;
+	char* info;
+	struct palavras* prox;
+} Palavras;
+
 typedef struct linhas{
 	struct linhas* ant;
 	int id;
-	char* info;
+	struct palavras* info;
 	struct linhas* prox;
 } Linhas;
 
